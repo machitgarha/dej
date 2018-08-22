79209582b93df6380aed45abbaa76d808338056e
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2018-08-22 21:14:53 +0430

    Prevent from getting a non-exist directory
    This caused fatal errors, when the user is trying to use the
    program for the first time. Because a class is trying to get
    directory list of a directory which doesn't exist.

--- includes/compare_files.php
+++ includes/compare_files.php
@@ -3,17 +3,21 @@
 // Replace a broken file with the backup
 function compare_files(string $path, string $backupDir) {
     // Get files info
+    directory($path);
     $files = new DirectoryIterator($path);
+
+    // Add path to backup directory
+    $backupDir = "$path/$backupDir";
 
     // Perform on all files
     foreach ($files as $file) {
         // Get names and paths
         $filename = $file->getFilename();
         $filePath = "$path/$filename";
-        $backupFilePath = "$path/$backupDir/$filename";
+        $backupFilePath = "$backupDir/$filename";
 
         // Check for a broken file, and replace it, if needed
-        if (file_exists($backupFilePath) &&
+        if (is_dir($backupDir) && file_exists($backupFilePath) &&
         get_num($backupFilePath) > get_num($filePath)) {
             // Remove the broken file
             unlink($filePath);
