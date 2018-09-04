3458f386f17acd2cde19647ba2d4caec216c77be
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2018-09-04 10:20:57 +0430

    Improve stop.sh
    Now it checks for root permissions, and also output message around
    it to let user know what is happening.

--- stop.sh
+++ stop.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-# Close all screens related to this project
-for session in $(screen -ls | grep -o '[0-9]*\.dej'); do
-    screen -S "${session}" -X quit;
-done
+# Check for root permissions
+if [ `whoami` = "root" ]; then
+    echo "Stopping everything... ";
+
+    # Close all screens related to this project
+    for session in `screen -ls | grep -o '[0-9]*\.dej'`; do
+        screen -S "${session}" -X quit;
+    done
+
+    echo "Done!";
+else
+    echo "Root permissions needed.";   
+fi
