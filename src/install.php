0064df192ed74829944e77ac09ed62aec136218f
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2018-11-17 01:16:36 +0330

    Improve installation steps
    Handle unexpected errors and exceptions. Also, prevent from
    installing when Dej already installed. And some other code
    improvements.

--- src/install.php
+++ src/install.php
@@ -3,46 +3,63 @@
 // Include all include files
 require_once "./includes/autoload.php";
 
-// Force to grant root permissions
-root_permissions();
+try {
+    // Force to grant root permissions
+    root_permissions();
 
-$sh->echo("Preparing to install Dej...");
+    $sh->echo("Preparing to install Dej...");
 
-// Data path
-$dataPath = __DIR__ . "/../";
-chdir($dataPath);
+    // Data path
+    $dataPath = __DIR__ . "/../";
+    chdir($dataPath);
 
-// Extract $PATH info to install there
-$defaultInstallPath = "/usr/local/bin";
-$paths = explode(":", `echo \$PATH`);
-if (in_array($defaultInstallPath, $paths))
-    $installPath = $defaultInstallPath;
-else
+    // Extract $PATH info and set installation path
+    $defaultInstallPath = "/usr/local/bin";
+    $paths = explode(":", `echo \$PATH`);
+    // Break if install path cannot be specified
+    if (empty($paths))
+        throw new Exception("Unknown installation path.");
     $installPath = $paths[0];
+    if (in_array($defaultInstallPath, $paths))
+        $installPath = $defaultInstallPath;
 
-$sh->echo("Preparing command file...");
+    $sh->echo("Preparing command file...");
 
-// Edit the source line of the Dej file to match with the current path
-$dejFileContentLines = explode(PHP_EOL, file_get_contents("dej"));
-foreach ($dejFileContentLines as $key => $line)
-    if ($line === "# SOURCE") {
-        $dejFileContentLines[$key + 1] = "cd \"$dataPath\"";
-        break;
-    }
+    // Edit the source line of the Dej file to match with the current path
+    $dejFile = new SplFileObject("dej", "r");
+    $dejFileContentLines = explode(PHP_EOL, $dejFile->fread($dejFile->getSize()));
+    foreach ($dejFileContentLines as $key => $line)
+        if ($line === "# SOURCE") {
+            $dejFileContentLines[$key + 1] = "cd \"$dataPath\"";
+            break;
+        }
 
-// Create a temporary command file matching new changes 
-$tmpFilename = "dej.tmp";
-$newFileContents = implode(PHP_EOL, $dejFileContentLines);
-$dejTmpFile = new SplFileObject($tmpFilename, "w");
-$dejTmpFile->fwrite($newFileContents);
-`sleep 1`;
+    // Create a temporary command file matching new changes
+    $tmpFile = "dej" . time() . ".tmp";
+    $newFileContents = implode(PHP_EOL, $dejFileContentLines);
+    $dejTmpFile = new SplFileObject($tmpFile, "w");
+    $dejTmpFile->fwrite($newFileContents);
 
-$sh->echo("Installing...");
+    $sh->echo("Installing...");
 
-// Move the temporary file to install path
-$dejInstalledFile = force_end_slash($installPath) . "dej";
-copy($tmpFilename, $dejInstalledFile);
-unlink($tmpFilename);
+    // The temporary file path to install
+    $dejInstallationFilePath = force_end_slash($installPath) . "dej";
+    
+    // Prevent from overwriting an older version
+    $isInstalled = file_exists($dejInstallationFilePath);
 
-// Grant right permissions
-chmod($dejInstalledFile, 0755);
+    // Move the temporary file, if not installed
+    if (!$isInstalled)
+        copy($tmpFile, $dejInstallationFilePath);
+    unlink($tmpFile);
+
+    if ($isInstalled)
+        $sh->error("Already installed.");
+
+    // Grant right permissions
+    chmod($dejInstalledFile, 0755);
+
+    $sh->echo("Completed. Type 'dej help' for more information.");
+} catch (Throwable $e) {
+    $sh->error($e);
+}
