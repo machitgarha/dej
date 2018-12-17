9fec161013aa425e6b0edb88f6c36e57fa6f43c5
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2018-12-17 21:42:27 +0330

    Fix a bug
    - Exit when the device was not set up.

--- src/tcpdump.php
+++ src/tcpdump.php
@@ -27,5 +27,7 @@ $tcpdump = $config->executables->tcpdump;
 * Uses TCPDump to sniff network packets. Official TCPDump site: www.tcpdump.org
 * -i: Sniffing the selected interface
 * -w: Write to the file
+* Use the loop for when the device is not set up
 */
-`$tcpdump -i $interfaceName -C 1 -w $tcpdumpLog`;
+while (true)
+    `$tcpdump -i $interfaceName -C 1 -w $tcpdumpLog`;
