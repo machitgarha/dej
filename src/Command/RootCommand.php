abdafc10abe2775b0ff28e6b2476fa561f0017ab
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2019-01-26 00:59:33 +0330

    Add RootCommand class
    An abstract class extending BasicCommand to force user to grant
    root permissions.

---
+++ src/Command/RootCommand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Dej\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+abstract class RootCommand extends BasicCommand
+{
+    public function __construct(string $name = null)
+    {
+        if (posix_getuid() !== 0)
+            throw new \Exception("Root permission needed", 1);
+
+        parent::__construct($name);
+    }
+}
