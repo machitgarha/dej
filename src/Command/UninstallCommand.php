a2bb615d9ee60821e0053406d915532da3b95275
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2019-02-03 06:22:01 +0330

    Add 'dej uninstall'
    From previous code, but with better functionality.

---
+++ src/Command/UninstallCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Dej\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+
+class UninstallCommand extends BaseCommand
+{
+    protected function configure()
+    {
+        $this->setName("uninstall");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->checkRootPermissions();
+
+        try {
+            $this->sh->echo("Preparing to uninstall Dej...");
+
+            // Find where Dej has been installed
+            $installationPath = trim(`which dej`);
+            if (empty($installationPath))
+                $this->sh->error("Not installed yet.");
+
+            // Get agreement
+            $helper = $this->getHelper("question");
+            $question = new ConfirmationQuestion("Are you sure? [N(o)/y(es)] ", false);
+            if (!$helper->ask($input, $output, $question)) {
+                $this->sh->exit("Aborted.");
+            }
+
+            $this->sh->echo("Uninstalling...");
+
+            // Grant right permissions to be able to remove it
+            chmod($installationPath, 0755);
+
+            // Remove the file
+            unlink($installationPath);
+
+            $this->sh->echo("Uninstalled successfully.");
+        } catch (Throwable $e) {
+            $this->sh->error($e);
+        }
+    }
+}
