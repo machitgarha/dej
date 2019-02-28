976ef3d927d620c21c6523e63881f97bbac7960a
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2019-02-28 21:35:51 +0330

    Add PHPDoc comments

--- src/Command/StatusCommand.php
+++ src/Command/StatusCommand.php
@@ -1,14 +1,25 @@
 <?php
+/**
+ * Dej command files.
+ * 
+ * @author Mohammad Amin Chitgarha <machitgarha@outlook.com>
+ * @see https://github.com/MAChitgarha/Dej
+ */
 
 namespace Dej\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Output\ConsoleOutput;
+use Dej\Element\ShellOutput;
 
+/**
+ * Gets Dej status.
+ */
 class StatusCommand extends BaseCommand
 {
+    /** @var int Number of screens when Dej is running successfully. */
     const SCREEN_NUMBER = 4;
+
     const STATUS_STOPPED = 0;
     const STATUS_RUNNING = 1;
     const STATUS_PARTIAL = 2;
@@ -23,11 +34,18 @@ class StatusCommand extends BaseCommand
         ;
     }
 
+    /**
+     * Executes status command.
+     *
+     * @param InputInterface $input
+     * @param ShellOutput $output
+     * @return void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->forceRootPermissions($output);
 
-        // Search for Dej screens
+        // Show message based on Dej status
         switch (self::getStatus()) {
             case self::STATUS_STOPPED:
                 $output->writeln("Not running.");
@@ -47,6 +65,11 @@ class StatusCommand extends BaseCommand
         }           
     }
 
+    /**
+     * Returns running screens by their names.
+     *
+     * @return array Array of screen name which are running.
+     */
     protected static function getRunningScreens(): array
     {
         // Wipes all dead screens
@@ -55,9 +78,8 @@ class StatusCommand extends BaseCommand
         // Lists screens
         $screens = `screen -ls`;
 
-        // Search for Dej screens
+        // Search for Dej screens and convert them to lowercase
         preg_match_all("/[0-9a-z]*\.dej/i", $screens, $matches, PREG_PATTERN_ORDER);
-
         array_walk($matches[0], function (&$val) {
             $val = strtolower(str_replace(".dej", "", $val));
         });
@@ -65,12 +87,22 @@ class StatusCommand extends BaseCommand
         return $matches[0];
     }
 
+    /**
+     * Returns the number of running processes.
+     *
+     * @return int The number of running processes of Dej.
+     */
     public static function getRunningScreensCount(): int
     {
         return count(self::getRunningScreens());
     }
 
-    public static function getStatus()
+    /**
+     * Returns the status of Dej.
+     *
+     * @return void One of the STATUS_* constants.
+     */
+    public static function getStatus(): int
     {
         $screensCount = self::getRunningScreensCount();
 
@@ -87,7 +119,13 @@ class StatusCommand extends BaseCommand
             return self::STATUS_OVERFLOW;
     }
 
-    public static function isRunning(string $process)
+    /**
+     * Tells whether a process is running or not.
+     *
+     * @param string $process The process name to be checked.
+     * @return bool
+     */
+    public static function isRunning(string $process): bool
     {
         return in_array(strtolower($process), self::getRunningScreens());
     }
