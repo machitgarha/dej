b8e1674fdc4ae4f85a29f01661afd59b8b148128
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2019-02-28 21:09:18 +0330

    Add PHPDoc comments

--- src/Command/ListCommand.php
+++ src/Command/ListCommand.php
@@ -1,11 +1,21 @@
 <?php
+/**
+ * Dej command files.
+ * 
+ * @author Mohammad Amin Chitgarha <machitgarha@outlook.com>
+ * @see https://github.com/MAChitgarha/Dej
+ */
 
 namespace Dej\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\Table;
+use Dej\Element\ShellOutput;
 
+/**
+ * Lists all commands.
+ */
 class ListCommand extends BaseCommand
 {
     protected function configure()
@@ -16,11 +26,22 @@ class ListCommand extends BaseCommand
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    /**
+     * Executes list command.
+     *
+     * @param InputInterface $input
+     * @param ShellOutput $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, $output)
     {
         $output->writeln("List of available commands:");
         $commands = $this->getApplication()->all();
 
+        /*
+         * Returns data for commands for a table, that contains command names and their
+         * descriptions.
+         */
         $getCommandListRows = function ($commands) {
             $rows = [];
             foreach ($commands as $command) {
@@ -33,6 +54,7 @@ class ListCommand extends BaseCommand
             return $rows;
         };
 
+        // Create a compact table of commands with their descriptions
         $listTable = new Table($output);
         $listTable->setRows($getCommandListRows($commands));
         $listTable->setStyle("compact")->render();
