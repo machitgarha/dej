047fb54af81ba53199ca3397c2d549f8177ba26e
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2019-02-28 18:26:21 +0330

    Add PHPDoc comments and improvements
    Add DocComments to ConfigCommand.php. Improve it, also, and fix
    some bugs related to the output.

--- src/Command/ConfigCommand.php
+++ src/Command/ConfigCommand.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Dej command files.
+ * 
+ * @author Mohammad Amin Chitgarha <machitgarha@outlook.com>
+ * @see https://github.com/MAChitgarha/Dej
+ */
 
 namespace Dej\Command;
 
@@ -15,83 +21,92 @@ use Symfony\Component\Console\Helper\TableStyle;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Output\NullOutput;
 
+/**
+ * Configures Dej.
+ * 
+ * For more details, see the command's help.
+ */
 class ConfigCommand extends BaseCommand
 {
     protected function configure()
     {
         $this
             ->setName("config")
-            ->addArgument("index", InputArgument::OPTIONAL)
+            ->addArgument("option", InputArgument::OPTIONAL)
             ->addArgument("value", InputArgument::OPTIONAL)
             ->setDescription("Configures Dej.")
             ->setHelp($this->getHelpFromFile("config"))
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    /**
+     * Executes config command.
+     *
+     * @param InputInterface $input
+     * @param ShellOutput $output
+     * @return void
+     * @throws \Exception If passing the first argument (i.e. option) is empty.
+     * @throws \Exception If the configuration file contains invalid JSON.
+     * @throws \RuntimeException When the JSON file contents are invalid.
+     */
+    protected function execute(InputInterface $input, $output)
     {
-        $index = $input->getArgument("index");
+        $firstArgument = $input->getArgument("option");
         $value = $input->getArgument("value");
 
-        if ($index === "list") {
+        // Handling 'dej config list' command
+        if ($firstArgument === "list") {
             $this->printOptionsList($output);
             return;
         }
 
-        if ($index === "?") {
+        // Handling 'dej config ? [option]'
+        if ($firstArgument === "?") {
             $this->getOptionDetails($value, $output);
             return;
         }
 
-        if ($index === "check") {
+        // Handling 'dej config check'
+        if ($firstArgument === "check") {
             $this->getApplication()->find("check")->run(new ArrayInput([]), $output);
             return;
         }
 
-        if (empty($index))
+        if (empty($firstArgument))
             throw new \Exception("Bad usage.");
 
-        // Load configurations
-        $loaded = true;
         try {
             $dataJson = $this->loadJson("data");
-        // If file doesn't exist, attempt to create it
         } catch (\Throwable $e) {
-            // Create the configuration file
-            try {
-                $this->createConfigFile($output);
-            } catch (\Throwable $e) {
-                $output->writeln($e->getMessage());
-            }
-
-            // Load it
-            $dataJson = $this->loadJson("data");
+            throw new \Exception("Invalid configuration file detected.");
         }
 
+        // Handling 'dej config [option]'
         if ($value === null) {
-            if ($dataJson->isSet($index)) {
-                $output->writeln($dataJson->get($index));
+            if ($dataJson->isSet($firstArgument)) {
+                $output->writeln($dataJson->get($firstArgument));
             } else {
                 $output->writeln([
-                    "Option '$index' does not exist.",
-                    "Run 'dej config list' for more info."
+                    "Option '$firstArgument' is not set.",
+                    "You can set it by running 'dej config $firstArgument [value]'."
                 ]);
             }
             return;
         }
 
-        // Load all possible options
+        // From here, handling 'dej config [option] [value]'
+        $option = $firstArgument;
+
+        // Load all available options
         $types = $this->loadJson("type", "data/validation")->get("data\.json");
-
-        // Extract all possible options
-        $possibleOptions = [];
+        $availableOptions = [];
         foreach ((array)$types as $key => $val)
-            array_push($possibleOptions, $key);
+            array_push($availableOptions, $key);
 
-        // Break if it is an invalid option
-        if (!in_array($index, $possibleOptions)) {
+        // If the option is not available in options list
+        if (!in_array($option, $availableOptions)) {
             $output->writeln([
-                "There is no '$index' option exists.",
+                "Invalid option '$firstArgument'.",
                 "Run 'dej config list' for more info."
             ]);
             return;
@@ -99,11 +114,11 @@ class ConfigCommand extends BaseCommand
 
         $output->writeln("Updating configurations...");
 
-        // Get field's current value
-        $currentValue = $dataJson->get($index);
+        // Get option's current value
+        $currentValue = $dataJson->get($firstArgument);
 
         // Fix values
-        $type = $types->$index->type ?? "string";
+        $type = $types->$option->type ?? "string";
         switch ($type) {
             case "bool":
                 $value = filter_var($value, FILTER_VALIDATE_BOOLEAN);
@@ -116,33 +131,43 @@ class ConfigCommand extends BaseCommand
             case "alphanumeric":
                 $value = preg_replace("/[^a-z0-9]/i", "", $value);
                 break;
-            
+
+            /*
+             * As a MAC address cannot be fixed, just alert user if it's invalid.
+             * Also, convert a dash-styled MAC address to colon-styled one.
+             */
             case "mac":
-                if (!preg_match("/^([\da-f]{2}:){5}([\da-f]{2})$/i", $value))
-                    $output->error("Wrong MAC address was given.");
+                if (!preg_match("/^([\da-f]{2}[:-]){5}([\da-f]{2})$/i", $value))
+                    throw new \RuntimeException("Wrong MAC address was given.");
+                
+                $value = str_replace("_", ":", $value);
                 break;
         }
 
-        // Change field's value
-        $dataJson->set($index, $value);
-
-        // Open the file to save
+        // Update the value and save the file
+        $dataJson->set($option, $value);
         $dataJson->save();
 
-        $output->writeln("Done!");
+        $output->write("Done! ");
+
+        // Print the changes, if there were any
         if ($currentValue !== null && $currentValue !== $value)
-            $output->writeln(json_encode($currentValue) . " -> " . json_encode($value));
+            $output->write("(" . json_encode($currentValue) . " => " . json_encode($value) . ")");
+
+        $output->writeln([
+            "",
+            ""
+        ]);
 
         // Restart Dej to see the effects and show the result, if root permissions granted
         try {
             $this->forceRootPermissions(new NullOutput());
-            $output->writeln("");
             $this->getApplication()->find("restart")->run(new ArrayInput([]), $output);
         } catch (\Throwable $e) {
             $output->warn("You have to restart Dej to see effects.");
         }
 
-        // Check for warnings
+        // Checks configuration file for warnings
         $warnings = DataValidation::new($this->loadJson("data"))
             ->classValidation()
             ->typeValidation()
@@ -159,72 +184,89 @@ class ConfigCommand extends BaseCommand
         }
     }
 
-    private function createConfigFile(OutputInterface $output)
+    /**
+     * Loads configuration options data, as an XML data.
+     *
+     * @return \SimpleXMLElement Configuration options data.
+     * @throws \Exception When something goes wrong on fetching options data.
+     */
+    private function loadConfigOptionsData(): \SimpleXMLElement
     {
-        $output->writeln("Creating...");
+        try {
+            $file = new \SplFileObject(__DIR__ . "/../../data/helps/config-list.xml", "r");
+            $configListXML = $file->fread($file->getSize());
+            $file = null;
+        } catch (\Throwable $e) {
+            throw new \Exception("Cannot fetch the list of options.");
+        }
 
-        // Create directory if it does not exist
-        Pusheh::createDir("config");
-        $dataJsonFile = "config/data.json";
-
-        if (file_exists($dataJsonFile))
-            throw new \Exception("Configuration file exists.");
-
-        // Create the configuration
-        touch($dataJsonFile);
-
-        // Make right permissions
-        chmod($dataJsonFile, 0755);
-
-        $output->echo("Done!");
+        return new \SimpleXMLElement($configListXML);
     }
 
+    /**
+     * Print a list of available options.
+     *
+     * List the options based on terminal width. If it's smaller than 60, then the list of options' names will be printed only; otherwise, if the screen is large enough, then the options will be printed with more details (i.e. with descriptions and default values) in a table. The table will also be styled based on the terminal width.
+     *
+     * @param ShellOutput $output
+     * @return void
+     */
     private function printOptionsList(ShellOutput $output)
     {
-        $configListFile = __DIR__ . "/../../data/helps/config-list.xml";
-        $configListXML = new \SimpleXMLElement(file_get_contents($configListFile));
+        $configListXML = $this->loadConfigOptionsData();
 
         $shellWidth = ShellOutput::getShellWidth();
         if ($shellWidth < 60) {
             $output->warn("Your terminal is too small. Available options:");
             foreach ($configListXML->option as $option)
-                $output->writeln($option->index);
+                $output->writeln(" " . $option->name);
         } else {
+            // 40: The approximate width of the names and default values columns
             $descriptionWidth = $shellWidth - 40;
 
-            $rows = [];
+            $tableRows = [];
             foreach ($configListXML->option as $option) {
-                $rows[] = [
-                    $option->index,
+                $tableRows[] = [
+                    $option->name,
                     ShellOutput::limitLines(trim($option->description), $descriptionWidth),
                     $option->default ?? "",
                 ];
             }
 
+            // Disable line limit, because limiting table is not a good idea
             $output->disableLineLimit();
+
+            // Create the table
             $table = new Table($output);
             $table->setHeaders([
                 "Name",
                 "Description",
                 "Default"
             ]);
-            $table->setRows($rows);
+            $table->setRows($tableRows);
             $table->setStyle("box")->render();
+
             $output->enableLineLimit();
         }
 
         $output->writeln("For more details on each option, try 'dej config ? [option]'.");
     }
 
+    /**
+     * Get detailed information about a configuration option.
+     *
+     * @param string $optionName The name of the option to fetch.
+     * @param ShellOutput $output
+     * @return void
+     */
     private function getOptionDetails(string $optionName, ShellOutput $output)
     {
-        $configListFile = __DIR__ . "/../../data/helps/config-list.xml";
-        $configListXML = new \SimpleXMLElement(file_get_contents($configListFile));
+        $configListXML = $this->loadConfigOptionsData();
 
         foreach ($configListXML->option as $option) {
-            if ($option->index[0] == $optionName) {
+            if ($option->name[0] == $optionName) {
                 $output->writeln([
-                    "Name: {$option->index}",
+                    "Name: {$option->name}",
                     "Description: " . trim($option->description),
                     "Default value: " . ($option->default[0] ?? "None")
                 ]);
